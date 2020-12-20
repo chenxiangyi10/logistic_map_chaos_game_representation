b423bbb23af39b381d84ef8619f7030c7d56096f
chenxiangyi10 <51980346+chenxiangyi10@users.noreply.github.com>
2020-12-20 09:08:29 -0500

    Add files via upload

--- pyplot1.py
+++ pyplot1.py
@@ -16,7 +16,7 @@ L0, d = poss0.shape
 L,n,d = poss2.shape
 
 
-fig = plt.figure(figsize=(7.5, 4.6))
+fig = plt.figure(figsize=(9.5, 5.6))
 ax = fig.add_subplot(231)
 ax2 = fig.add_subplot(232)
 ax3 = fig.add_subplot(233)
@@ -25,7 +25,8 @@ ax5 = fig.add_subplot(235)
 ax6 = fig.add_subplot(236)
 axs = [ax, ax2, ax3, ax4, ax5, ax6]
 
-mu = [2.6, 3.4, 3.6, 3.8, 3.9, 4.0]
+mu = [2.6, 3.4, 3.5, 3.8, 3.9, 4.0]
+cycle = ["1 cycle", "2 cycles", "4 cycles", "chaos", "chaos", "chaos"]
 index = []
 for value in mu:
     index.append(list(pvalues).index(value))
@@ -34,8 +35,8 @@ for i in range(6):
     axs[i].scatter(poss0[:50000,0], poss0[:50000,1], s = 0.5, color = "blue", marker = "o", alpha = 0.002)
     axs[i].set_xlabel("$x$")
     axs[i].set_ylabel("$y$")
-    axs[i].scatter(poss2[index[i],:,0], poss2[index[i],:,1], s=0.5, color = "black", marker = "o", alpha = 0.05, label="$μ=%f$"%mu[i])
-    axs[i].set_title("$μ=%f$"%mu[i])
+    axs[i].scatter(poss2[index[i],:,0], poss2[index[i],:,1], s=0.5, color = "black", marker = "o", alpha = 0.05, label="$μ=%.2f$"%mu[i])
+    axs[i].set_title("$μ=%.2f$, regime = %s"%(mu[i],cycle[i]))
     #axs[i].legend(frameon=False, loc = 1)
 plt.tight_layout()
 print("done")
